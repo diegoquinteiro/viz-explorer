41a8d1bf28d6b8f1dcc6d23a93390d8ad5940d02
Diego Moreno Quinteiro <diegoquinteiro@fb.com>
2022-12-27 18:23:00 -0300

    Simplify colors on theme, extract to SCSS

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,11 +1,8 @@
-import './index.css';
+import './theme/dark.scss';
 import 'svg-pan-zoom-container'
 import Page from "./react/Page";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(React.createElement(Page));
